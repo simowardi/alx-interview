e102d3514bd0ff5f39628210914be17bdad8a095
simowardi <daoist32@gmail.com>
2024-08-02 12:39:01 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-"""
-Determines if all the boxes can be opened.
-"""
+'''LockBoxes Challenge'''
+
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be opened.
+    '''
+    Determines if all the boxes can be opened or not.
 
     Args:
-        boxes (list of lists): A list of lists where each inner list represents the
-        keys in a box.
+        boxes (list of lists): A list of lists, where each inner list represents
+        the keys in a box.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
-    """
-    # Initialize variables
+    '''
+    # Get the total number of boxes
     num_boxes = len(boxes)
-    opened_boxes = 1  # Start with box 0 opened
-    current_box = 0
 
-    # Use a while loop to open as many boxes as possible
-    while current_box < num_boxes:
-        # Get the keys in the current box
-        keys = boxes[current_box]
+    # Initialize a set to store the keys and a list to store the opened boxes
+    keys = set()
+    opened_boxes = [0]  # Start with box 0 opened
+
+    # Iterate through the opened boxes
+    i = 0
+    while i < len(opened_boxes):
+        current_box = opened_boxes[i]
+
+        # Add the keys in the current box to the keys set
+        keys.update(boxes[current_box])
 
         # Try to open new boxes with the keys
         for key in keys:
-            if key < num_boxes and key not in boxes[key]:
-                opened_boxes += 1
-                boxes[key] = []  # Mark the box as opened by emptying its keys
+            if key != 0 and key < num_boxes and key not in opened_boxes:
+                opened_boxes.append(key)
 
-        # Move to the next unopened box
-        current_box += 1
+        # Move to the next opened box
+        i += 1
 
     # Check if all boxes have been opened
-    return opened_boxes == num_boxes
+    return len(opened_boxes) == num_boxes
