0e4e4501042fded9d01297c0d4ca8d7cd844b9d1
simowardi <daoist32@gmail.com>
2024-08-08 14:33:38 +0000

    minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
-"""
-Module for calculating minimum operations to achieve n H characters.
-"""
+'''The minimum operations coding challenge.
+'''
 
 
 def minOperations(n):
-    """
-    Calculates the fewest number of operations needed to result in exactly n H characters.
-    
-    Args:
-    n (int): The desired number of H characters
-    
-    Returns:
-    int: The minimum number of operations, or 0 if n is impossible to achieve
-    """
-    if not isinstance(n, int) or n <= 1:
-        return 0
+   '''Computes the fewest number of operations needed to result
+   in exactly n H characters.
+   '''
+   if not isinstance(n, int):
+       return 0
 
-    operations = 0
-    current_h = 1
-    
-    for i in range(2, n + 1):
-        while n % i == 0:
-            operations += i
-            n //= i
-        
-        if n == 1:
-            break
+   # Counter for the number of operations performed
+   operation_count = 0
+   # Stores the number of H's that can be copied and pasted
+   buffer = 0
+   # Represents the current number of H's in the file
+   current_h = 1
 
-    return operations
+   while current_h < n:
+       if buffer == 0:
+           # Perform the initial Copy All and Paste operations to start building the H's
+           buffer = current_h
+           current_h += buffer
+           operation_count += 2
+       elif n - current_h > 0 and (n - current_h) % current_h == 0:
+           # Perform Copy All and Paste operations when it's more efficient to do so, 
+           # to increase the number of H's in the file
+           buffer = current_h
+           current_h += buffer
+           operation_count += 2
+       elif buffer > 0:
+           # Perform a Paste operation to add the contents of the buffer to the current H's
+           current_h += buffer
+           operation_count += 1
+
+   return operation_count
