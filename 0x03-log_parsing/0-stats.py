3164169822636ac1788760f1f3e21f150cb57624
simowardi <daoist32@gmail.com>
2024-08-15 17:15:12 -0700

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,35 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics"""
-
+'''A script that reads stdin line by line and computes metrics'''
 import sys
 
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0}
+total_file_size = 0
+line_count = 0
 
-def print_stats(total_size, status_codes):
-    """Print accumulated statistics"""
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
-
-
-def main():
-    total_size = 0
-    status_codes = {
-        200: 0, 301: 0, 400: 0, 401: 0,
-        403: 0, 404: 0, 405: 0, 500: 0
-    }
-    line_count = 0
-
-    try:
-        for line in sys.stdin:
+try:
+    for line in sys.stdin:
+        line_parts = line.split()
+        if len(line_parts) > 4:
+            status_code = line_parts[-2]
+            file_size = int(line_parts[-1])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            total_file_size += file_size
             line_count += 1
-            parts = line.split()
 
-            if len(parts) > 2:
-                try:
-                    status_code = int(parts[-2])
-                    file_size = int(parts[-1])
+        if line_count == 10:
+            line_count = 0
+            print('File size: {}'.format(total_file_size))
+            for code, count in sorted(status_codes.items()):
+                if count != 0:
+                    print('{}: {}'.format(code, count))
 
-                    if status_code in status_codes:
-                        status_codes[status_code] += 1
-                    total_size += file_size
-                except ValueError:
-                    pass
+except KeyboardInterrupt:
+    pass
 
-            if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-        raise
-
-    print_stats(total_size, status_codes)
-
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_file_size))
+    for code, count in sorted(status_codes.items()):
+        if count != 0:
+            print('{}: {}'.format(code, count))
