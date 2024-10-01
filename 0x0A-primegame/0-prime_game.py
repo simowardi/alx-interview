a6cb32c007815e835a4ed990429af8d76684a54e
simowardi <daoist32@gmail.com>
2024-10-01 21:17:15 +0000

    primegame initial commit

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,21 +8,21 @@ def isWinner(x, nums):
     """Determines the winner after `x` rounds.
     Args:
         x (int): Number of rounds.
-        nums (list): Array of `n` values for each round.    
+        nums (list): Array of `n` values for each round.
     Returns:
         str: Name of the player with the most wins,
         or None if a tie.
     """
     if x < 1 or not nums:
         return None
-    
+
     marias_wins, bens_wins = 0, 0
     n = max(nums)
 
     # Generate prime numbers up to the max value in nums
     primes = [True] * (n + 1)
     primes[0] = primes[1] = False
-    
+
     for i in range(2, n + 1):
         if primes[i]:
             for j in range(i * 2, n + 1, i):
@@ -31,7 +31,7 @@ def isWinner(x, nums):
     # Count primes in each round
     for round_num in nums:
         prime_count = sum(primes[1:round_num + 1])
-        
+
         if prime_count % 2 == 0:
             bens_wins += 1
         else:
